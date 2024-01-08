d1a6d71a8de8578b691f3e28e9147c783208bc43
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-08 19:06:39 -0300

    feat: Interface Implemented in Customer Repository

---
+++ CustomerFlow/Infra/CustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using CustomerFlow.Model;
+
+namespace CustomerFlow.Infra
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public void Add(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
