c83f6c7e169c5321ab5d1c42d85ba16ebe79a208
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-08 19:04:01 -0300

    Feat: Db Set String connection

---
+++ CustomerFlow/Infra/ConnectionContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomerFlow.Model;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace CustomerFlow.Infra
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet <Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(
+                "Server:localhost;"+
+                "Port=8080; Database=customer;"+
+                "User Id= luiz;"+
+                "Password=11072001;");
+    }
+}
