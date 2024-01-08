b7edd0f20e41f1328c7220376fdb73cb1baa2244
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-08 20:28:46 -0300

    Feat: Customer Controller Constructor Repository

--- CustomerFlow/Controller/CustomerController.cs
+++ CustomerFlow/Controller/CustomerController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CustomerFlow.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CustomerFlow.Controller
 {
-    public class CustomerController : Controller
+    [ApiController]
+    [Route("api/vi/customer")]
+    public class CustomerController : ControllerBase
     {
+
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
