f752b28e71e814439713f4f96250d652a7c8d745
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-08 18:43:58 -0300

    Feat: Customer Entity

---
+++ CustomerFlow/Model/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerFlow.Model
+{
+    public class Customer
+    {
+        [Key]
+        public int id { get; private set; }
+        public string name { get; private set; }
+        public int age { get; private set; }
+
+        public string photo { get; private set; }
+    }
+}
