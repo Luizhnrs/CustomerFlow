e10909ba7d61c6203faaa36fa469d25d9cb0cce6
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-09 14:55:59 -0300

    Correction: Connection e Respository

--- CustomerFlow/Infra/ConnectionContext.cs
+++ CustomerFlow/Infra/ConnectionContext.cs
@@ -10,9 +10,9 @@ namespace CustomerFlow.Infra
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql(
-                "Server:localhost;"+
+                "Server=PostgreSQL 16;"+
                 "Port=8080; Database=customer;"+
-                "User Id= luiz;"+
-                "Password=11072001;");
+                "User Id=postgres;" +
+                "Password=;");
     }
 }
--- CustomerFlow/Infra/CustomerRepository.cs
+++ CustomerFlow/Infra/CustomerRepository.cs
@@ -4,11 +4,11 @@ namespace CustomerFlow.Infra
 {
     public class CustomerRepository : ICustomerRepository
     {
-        private readonly ConnectionContext _context= new ConnectionContext();
+        private readonly ConnectionContext _context = new ConnectionContext();
         public void Add(Customer customer)
         {
             _context.Customers.Add(customer);
-           _context.SaveChanges();
+            _context.SaveChanges();
         }
 
         public List<Customer> Get()
