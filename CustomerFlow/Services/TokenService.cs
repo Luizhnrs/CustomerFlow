70059b3a62484da01d56e937192753ec90f3fe33
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-10 15:10:44 -0300

    Feat: Token JWT Security Handler

--- CustomerFlow/Services/TokenService.cs
+++ CustomerFlow/Services/TokenService.cs
@@ -2,6 +2,7 @@
 using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace CustomerFlow.Services
 {
@@ -15,7 +16,12 @@ namespace CustomerFlow.Services
                 Subject = new System.Security.Claims.ClaimsIdentity(new Claim[] {
                        new Claim ("customerId", customer.id.ToString()),
                 }),
-            }
+                Expires = DateTime.UtcNow.AddHours(3),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.EcdsaSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenConfig);  
+            var tokenString = tokenHandler.WriteToken(token);
         }
     }
 }
