ffe3d6c993f99c3cfae1b33765c42a96d9ec5b9c
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-10 14:48:23 -0300

    Feat: Token Service

---
+++ CustomerFlow/Services/TokenService.cs
@@ -0,0 +1,12 @@
+﻿using CustomerFlow.Model;
+
+namespace CustomerFlow.Services
+{
+    public class TokenService
+    {
+        public static object GenerateToken (Customer customer)
+        {
+            
+        }
+    }
+}
