b50b2952db6c77952afd87ef3bd240cf3748997a
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-10 14:55:55 -0300

    Feat: Generate Token

--- CustomerFlow/Services/TokenService.cs
+++ CustomerFlow/Services/TokenService.cs
@@ -1,4 +1,7 @@
 ﻿using CustomerFlow.Model;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CustomerFlow.Services
 {
@@ -6,7 +9,13 @@ namespace CustomerFlow.Services
     {
         public static object GenerateToken (Customer customer)
         {
-            
+            var key = Encoding.ASCII.GetBytes(Key.Secret);
+            var tokenConfig = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[] {
+                       new Claim ("customerId", customer.id.ToString()),
+                }),
+            }
         }
     }
 }
